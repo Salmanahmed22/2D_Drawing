4de7d91e581d420d0f68b136ea92e3929b4ed1d7
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-05-28 19:00:15 +0300

    hermite curve

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -58,3 +58,32 @@ void Algorithms::InterpolatedColoredLine(HDC hdc, int x1, int y1, int x2, int y2
         SetPixel(hdc, Utils::Round(x), Utils::Round(y),c);
     }
 }
+
+//Curves
+void Algorithms::drawHermiteCurve(HDC hdc , int x1, int y1 , int u1, int v1 , int x2, int y2 , int u2, int v2, COLORREF c){
+    int hermiteMatrix[4][4] = {
+            { 2, -2,  1,  1 },
+            {-3,  3, -2, -1 },
+            { 0,  0,  1,  0 },
+            { 1,  0,  0,  0 }
+    };
+    int xc[4] = {x1,x2,u1,u2};
+    int yc[4] = {y1,y2,v1,v2};
+
+    double a[4], b[4];
+
+    for (int i = 0; i < 4; i++) {
+        a[i] = b[i] = 0;
+        for (int j = 0; j < 4; j++) {
+            a[i] += hermiteMatrix[i][j] * xc[j];
+            b[i] += hermiteMatrix[i][j] * yc[j];
+        }
+    }
+
+    for (double t = 0; t <= 1; t+=0.001) {
+        double x = a[0]*pow(t,3) + a[1]*pow(t,2) + a[2]*t + a[3];
+        double y = b[0]*pow(t,3) + b[1]*pow(t,2) + b[2]*t + b[3];
+        SetPixel(hdc, Utils::Round(x), Utils::Round(y),c);
+    }
+}
+
--- Algorithms.h
+++ Algorithms.h
@@ -5,5 +5,6 @@ public:
     static void DrawCircleModifiedMidpoint(HDC hdc, int xc, int yc, int R, COLORREF color);
     static void DrawEllipseDirect(HDC hdc, int xc, int yc, int a, int b, COLORREF c);
     static void InterpolatedColoredLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c);
+    static void drawHermiteCurve(HDC hdc , int x1, int y1 , int u1, int v1 , int x2, int y2 , int u2, int v2, COLORREF c);
 };
 
